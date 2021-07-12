4b6a861791e73458b1918276baedc5675f648a19
Vikram Damle <vikramdamle6@gmail.com>
2021-07-12 17:59:52 +0530

    Tasks 6 & 7
      - Add Login and Signup Pages (Task 6)
      - Implement communication between popup.js & background.js

--- extension/js/background.js
+++ extension/js/background.js
@@ -17,66 +17,23 @@ const rawData = {
     vegetables: []
 }
 
-const sendData = () => {
-    // const xhttp = new XMLHttpRequest()
-    // xhttp.open('POST', 'http://localhost:8000/products', true)
-    // xhttp.send(jsonData)
-    
-    // const jsonData = JSON.stringify(rawData);
-    
-
-    const postData = async (url = '', data = {}) => {
-        const resp = []
-        const response = await fetch(url, {
-            method: 'POST',
-            mode: 'cors',
-            cache: 'no-cache',
-            credentials: 'omit',
-            // headers: {
-            //     // 'Content-Type': 'application/json'
-            //     // 'Content-Type': 'application/x-www-form-urlencoded'
-            //     // 'Content-Type': 'text/plain'
-            // },
-            body: JSON.stringify(data)
-        })
-        // .then((response) => {
-        //     console.log(response)
-        //     return response.body.getReader()
-        // }).then((reader) => {
-        //     reader.read().then(({done, value}) => {
-        //         if(done) {
-        //             reader.close()
-        //             return
-        //         }
-        //         resp.push(value)
-        //     })
-        // })
-
-        console.log(response.headers)
-        return resp
-    }
-        
-    postData(url, rawData)
-    .then((data) => {
-        console.log(data)
-    })
-
-}
-
-
 chrome.runtime.onInstalled.addListener(() => {
     console.log("Installed Extension!");
     // sendData()
-    $.ajax({
-        url: url,
-        data: JSON.stringify(rawData),
-        type: 'POST',
-        success: (response) => {
-            console.log(('response: ', response))
-        },
-        error: (response) => {
-            console.log('error: ', response)
-        }
-    })
+    // $.ajax({
+    //     url: url,
+    //     data: JSON.stringify(rawData),
+    //     type: 'POST',
+    //     cache: 'no-cache',
+    //     success: (response) => {
+    //         console.log(('response: ', response))
+    //     },
+    //     error: (response) => {
+    //         console.log('error: ', response)
+    //     },
+    // })
 
+    chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+        console.log(JSON.stringify(sender) + ": " + request.username + " " + request.password)
+    })
 })
